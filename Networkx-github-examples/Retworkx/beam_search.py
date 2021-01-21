b95717fa2b0e4229c643055389419b41fc51aca7
FUG171V3 <gouravbajeli@outlook.com>
2021-01-21 22:15:45 +0530

    type annotation added

--- Networkx-github-examples/Retworkx/beam_search.py
+++ Networkx-github-examples/Retworkx/beam_search.py
@@ -2,8 +2,10 @@ import math
 import matplotlib.pyplot as plt
 import retworkx as rx
 import networkx as nx
+import numpy
+from typing import *
 
-def progressive_widening_search(G, source, value, condition, initial_width=1):
+def progressive_widening_search(G, source, value, condition, initial_width=1)->int :
     if condition(source):
         return source
     log_m=math.ceil(math.log2((len(G1))))
@@ -12,26 +14,30 @@ def progressive_widening_search(G, source, value, condition, initial_width=1):
         for u, v in nx.bfs_beam_edges(G1, source, value, width):
             if condition(v):
                 return v
-    return nx.NodeNotFound("no node")
+    print(nx.NodeNotFound("no node"))
 
-seed=89
-
-G=rx.undirected_gnp_random_graph(100,0.5,seed=seed)
-n1 = rx.graph_adjacency_matrix(G, None)
-G1 = nx.from_numpy_matrix(n1)
-centrality=nx.eigenvector_centrality(G1)
-avg_centrality = sum(centrality.values()) / len(G1)
-
-def has_high_centrality(v):
+def has_high_centrality(v:int) -> bool:
     return centrality[v] >= avg_centrality
 
-source=0
+seed: int = 89
+
+#conversion of retworkx graph to networkx couldnt find any func in retworkx to calculate centality
+G: rx.PyGraph =rx.undirected_gnp_random_graph(100,0.5,seed=seed)
+n1 : numpy.ndarray = rx.graph_adjacency_matrix(G, None)
+G1:nx.classes.graph.Graph = nx.from_numpy_matrix(n1)
+
+
+centrality: dict =nx.eigenvector_centrality(G1)
+avg_centrality:float = sum(centrality.values()) / len(G1)
+
+source: int =0
 value=centrality.get
-condition=has_high_centrality
-found_node=progressive_widening_search(G1,source,value,condition)
-c=centrality[found_node]
+condition: bool =has_high_centrality
+found_node:int =progressive_widening_search(G1,source,value,condition)
+c:float =centrality[found_node]
 print(f"found node {found_node} with centrality {c}")
 
+#plotting
 pos=nx.spring_layout(G1,seed=seed)
 options = {
     "node_color": "blue",
