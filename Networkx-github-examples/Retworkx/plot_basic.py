9a435314ee4c00f501f66fc7ded0cf6fb4783946
FUG171V3 <gouravbajeli@outlook.com>
2021-01-21 23:12:53 +0530

    type annotation added

--- Networkx-github-examples/Retworkx/plot_basic.py
+++ Networkx-github-examples/Retworkx/plot_basic.py
@@ -1,13 +1,15 @@
 import retworkx as rx
-#import networkx as nx
+import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-G=rx.generators.cycle_graph(20)
-pos=rx. #have to find a alternative for spring_layout
+G:rx.PyDiGraph=rx.generators.cycle_graph(20)
+n1 : np.ndarray = rx.graph_adjacency_matrix(G, None)
+G1:nx.classes.graph.Graph = nx.from_numpy_matrix(n1)
+pos=nx.spring_layout(G1,seed=None)  #have to find a alternative for spring_layout
 
-node_xyz = np.array([pos[v] for v in sorted(G)])
-edge_xyz = np.array([(pos[u], pos[v]) for u, v in G.edges()])
+node_xyz = np.array([pos[v] for v in sorted(G1)])
+edge_xyz = np.array([(pos[u], pos[v]) for u, v in G1.edges()])
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
